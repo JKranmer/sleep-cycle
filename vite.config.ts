189df35da99a30ecd449c3570d296188903c2a2f
Jonathas Kranmer <jonathas@vert-capital.com>
2023-03-20 18:04:26 -0300

    ajuste para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: "/sleep-cycle",
     plugins: [
       vue(),
       VitePWA({
